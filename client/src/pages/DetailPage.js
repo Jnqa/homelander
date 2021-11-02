17b26c2809d471824591b64f5ecf6a2a9dce5d13
Jnqa <ligez@yandex.ru>
2021-11-02 18:06:54 +0300

    blank slate

--- client/src/pages/DetailPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const DetailPage = () => {
-    return (
-        <div className="container">
-        <h1>DetailPage</h1>
-      </div>
-    )
-}
--- client/src/pages/LinksPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const LinksPage = () => {
-    return (
-        <div className="container">
-        <h1>LinksPage</h1>
-      </div>
-    )
-}
